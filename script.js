741508ddf65030f67df164e1c1506e07ea43ee94
Annabellovesyou <annabel.claij@hva.nl>
2025-11-21 11:30:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// JavaScript Document
-console.log("hi");
+
+
+const deButton = document.querySelector("header button");
+const deNav = document.querySelector("header nav");
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu () {
+  deButton.classList.toggle("is-open");
+  deNav.classList.toggle("is-open");
+}
+
